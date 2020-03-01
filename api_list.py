f01e75fe3ad71293add0924170b70ef29cbc380e
YaoCheng-Home <286858609@qq.com>
2020-03-01 12:56:42 +0800

    identity

---
+++ api_list.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# 都君丨大魔王
+import inspect
+import sys
+from PyQt5 import QtCore
+from public_methon import request_post
+
+
+class IdentityAPI(QtCore.QThread):
+    text = QtCore.pyqtSignal
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        ip = kwargs.get('ip')
+        port = kwargs.get('port')
+        self.url_prefix = f"http://{ip}:{port}"
+        self.username = kwargs.get('username')
+        self.password = kwargs.get('password')
+        self.keyword = kwargs.get('keyword')
+        self.search_rule = kwargs.get('search_rule')
+        self.execute_scope = kwargs.get('execute_scope')
+
+    def login(self):
+        url = f"{self.url_prefix}/call?id=experts.login&v="
+        data = {
+            'nickname': self.username,
+            'passwd': self.password
+        }
+        method_name = inspect.stack()[0][3]
+        res = request_post(method_name, url, data)
+        has_error = res.get('hasError')
+        if has_error is False:
+            session_id = res.get('data').get('sessionId')
+        else:
+            session_id = None
+        return session_id
+
+    def list_case(self, session_id, offset=0, limit=999, removeType=0, type=1):
+        url = f"{self.url_prefix}/call?id=experts.listCriminalCase&v="
+        data = {
+            "sessionid": session_id,
+            "offset": offset,
+            "limit": limit,
+            "removeType": removeType,
+            "key_word": [self.keyword],
+            "type": type
+        }
+        method_name = inspect.stack()[0][3]
+        res = request_post(method_name, url, data)
+        return res
+
+    def run(self):
+        ...
+
+
+if __name__ == "__main__":
+    info = {
+        "ip": "47.107.34.83",
+        "port": "20000",
+        "username": "yaocheng",
+        "password": "123456",
+        "keyword": "",
+        "search_rule": 0,
+        "execute_scope": 0
+    }
+    jd_api = IdentityAPI(**info)
+    session = jd_api.login()
+    jd_api.list_case(session)
---
+++ public_methon.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# 都君丨大魔王
+import json
+import requests
+import logging
+import os
+
+
+def get_file_path(project_name='JianDingCaseDel', file_name='catalina.log'):
+    """获得日志文件保存路径"""
+    sep = str(os.sep)
+    current_file_path = os.path.split(os.path.abspath(__file__))[0]
+    path_split = current_file_path.split(sep)
+    project_index = path_split.index(project_name)
+    need_path = path_split[:project_index + 1]
+    need_path.append(file_name)
+    finally_path = sep.join(need_path)
+    return finally_path
+
+
+def log_config():
+    log_path = get_file_path()
+    root_logger = logging.getLogger()
+    root_logger.setLevel('INFO')
+    basic_format = "%(asctime)s [%(levelname)s] %(message)s"
+    date_format = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter(basic_format, date_format)
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    fh = logging.FileHandler(log_path)
+    fh.setFormatter(formatter)
+    root_logger.addHandler(sh)
+    root_logger.addHandler(fh)
+
+
+log_config()
+
+
+def dict_format(dict_object):
+    format_object = json.dumps(dict_object, indent=4, ensure_ascii=False, separators=(',', ':'))
+    return format_object
+
+
+def request_post(method_name, url, req):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    logging.info(f"The {method_name} request data is:\n{dict_format(req)}")
+    res = requests.post(url, data=json.dumps(req), headers=headers).json()
+    logging.info(f"The {method_name} response data is:\n{dict_format(res)}")
+    return res
+
+
